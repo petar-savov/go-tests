4d22aa5cc06039ba42e274edde5ab314f1202074
petar-savov <p.savov@outlook.com>
2022-10-02 07:51:46 +0100

    Fix failing test literal

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -21,7 +21,7 @@ func TestArea(t *testing.T) {
 	}{
 		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
 		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 32.0},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
