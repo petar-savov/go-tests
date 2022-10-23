7ba17ab15e551095c4baae639463edc8244d5583
petar-savov <p.savov@outlook.com>
2022-10-23 18:45:23 +0100

    Add transaction functionality

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,13 +1,20 @@
 package main
 
 func Sum(numbers []int) int {
-	s := 0
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
+}
 
-	for _, n := range numbers {
-		s += n
+func SumAllTails(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
+		} else {
+			tail := x[1:]
+			return append(acc, Sum(tail))
+		}
 	}
-
-	return s
+	return Reduce(numbers, sumTail, []int{})
 }
 
 func SumAll(numbersToSum ...[]int) []int {
@@ -21,18 +28,29 @@ func SumAll(numbersToSum ...[]int) []int {
 	return sums
 }
 
-func SumAllTails(numbersToSum ...[]int) []int {
-
-	var sums []int
-
-	for _, numbers := range numbersToSum {
-		if len(numbers) == 0 {
-			sums = append(sums, 0)
-		} else {
-			sums = append(sums, Sum(numbers[1:]))
-		}
-
+func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = accumulator(result, x)
 	}
+	return result
+}
 
-	return sums
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	var balance float64
+	for _, t := range transactions {
+		if t.From == name {
+			balance -= t.Sum
+		}
+		if t.To == name {
+			balance += t.Sum
+		}
+	}
+	return balance
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -52,3 +52,67 @@ func TestSumAllTails(t *testing.T) {
 	})
 
 }
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Grace")
+	})
+
+}
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %v", got)
+	}
+}
+
+func TestBadBank(t *testing.T) {
+	transactions := []Transaction{
+		{
+			From: "Chris",
+			To:   "Riya",
+			Sum:  100,
+		},
+		{
+			From: "Adil",
+			To:   "Chris",
+			Sum:  25,
+		},
+	}
+
+	AssertEqual(t, BalanceFor(transactions, "Riya"), 100)
+	AssertEqual(t, BalanceFor(transactions, "Chris"), -75)
+	AssertEqual(t, BalanceFor(transactions, "Adil"), -25)
+}
